ba7cbd008558aafe442a354406b690a181e4f12a
weversonf <33959896+weversonf@users.noreply.github.com>
2026-06-19 02:06:02 -0300

    Add files via upload

---
+++ pro/sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'mu-finance-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icons/icon.svg',
+  'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.31.0/tabler-icons.min.css',
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore-compat.js',
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth-compat.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+
+  if (url.origin === location.origin && url.pathname === '/') {
+    e.respondWith(caches.match('index.html'));
+    return;
+  }
+
+  e.respondWith(
+    caches.match(request).then(res => res || fetch(request).then(res => {
+      if (res.ok && res.type === 'basic') {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(request, clone));
+      }
+      return res;
+    }))
+  );
+});
